ec9f03b46183226bbee84c201225c901f1db942f
Petecosh <prodrguez4@gmail.com>
2024-09-11 05:34:50 -0600

    Comando.sh

---
+++ comando.sh
@@ -0,0 +1,4 @@
+source /mnt/vol_NFS_rh003/estudiantes/archivos_config/synopsys_tools.sh;
+rm -rfv `ls |grep -v ".*\.sv\|.*\.sh"`;
+vcs -Mupdate test_bench.sv  -o salida -full64 -debug_all -kdb -sverilog -l log_test +lint=TFIPC-L;
+
