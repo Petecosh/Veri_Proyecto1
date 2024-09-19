c701075b86b22fb8324232fff0d34bca9b214375
Josue096 <josu3qb@gmail.com>
2024-09-19 17:42:53 -0600

    pruebita ;)

--- agente.sv
+++ agente.sv
@@ -11,14 +11,14 @@ class agente #(parameter devices = 4, parameter width = 16);
         forever begin
             #1
             if (test_agnt_mbx.num() > 0) begin
-                $display("[%g] Agente: Se recibe una instruccion", $time);
+                $display("[%g] Agente,%d: Se recibe una instruccion", $time,instruccion_agente.origen);
                 test_agnt_mbx.get(instruccion_agente);
                 case (instruccion_agente.tipo)
 
                     lectura: begin
                         paquete_agnt_drv[instruccion_agente.origen] = new();
                         paquete_agnt_drv[instruccion_agente.origen].tipo = instruccion_agente.tipo;
-                        paquete_agnt_drv[instruccion_agente.origen].print("Agente%d: Transaccion creada",instruccion_agente.origen);
+                        paquete_agnt_drv[instruccion_agente.origen].print("Agente: Transaccion creada");
                         agnt_drv_mbx[instruccion_agente.origen].put(paquete_agnt_drv[instruccion_agente.origen]);
                     end
 
@@ -26,7 +26,7 @@ class agente #(parameter devices = 4, parameter width = 16);
                         paquete_agnt_drv[instruccion_agente.origen] = new();
                         paquete_agnt_drv[instruccion_agente.origen].dato_i = instruccion_agente.dato;
                         paquete_agnt_drv[instruccion_agente.origen].tipo = instruccion_agente.tipo;
-                        paquete_agnt_drv[instruccion_agente.origen].print("Agente%d: Transaccion creada",instruccion_agente.origen);
+                        paquete_agnt_drv[instruccion_agente.origen].print("Agente: Transaccion creada");
                         agnt_drv_mbx[instruccion_agente.origen].put(paquete_agnt_drv[instruccion_agente.origen]);
                     end
                     
