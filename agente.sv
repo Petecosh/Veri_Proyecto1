89c536a2f7bbf41f47f38c6d366ff8a34375a83d
Josue096 <josu3qb@gmail.com>
2024-09-19 17:39:28 -0600

    pruebita ;)

--- agente.sv
+++ agente.sv
@@ -18,7 +18,7 @@ class agente #(parameter devices = 4, parameter width = 16);
                     lectura: begin
                         paquete_agnt_drv[instruccion_agente.origen] = new();
                         paquete_agnt_drv[instruccion_agente.origen].tipo = instruccion_agente.tipo;
-                        paquete_agnt_drv[instruccion_agente.origen].print("Agente: Transaccion creada");
+                        paquete_agnt_drv[instruccion_agente.origen].print("Agente%d: Transaccion creada",instruccion_agente.origen);
                         agnt_drv_mbx[instruccion_agente.origen].put(paquete_agnt_drv[instruccion_agente.origen]);
                     end
 
@@ -26,7 +26,7 @@ class agente #(parameter devices = 4, parameter width = 16);
                         paquete_agnt_drv[instruccion_agente.origen] = new();
                         paquete_agnt_drv[instruccion_agente.origen].dato_i = instruccion_agente.dato;
                         paquete_agnt_drv[instruccion_agente.origen].tipo = instruccion_agente.tipo;
-                        paquete_agnt_drv[instruccion_agente.origen].print("Agente: Transaccion creada");
+                        paquete_agnt_drv[instruccion_agente.origen].print("Agente%d: Transaccion creada",instruccion_agente.origen);
                         agnt_drv_mbx[instruccion_agente.origen].put(paquete_agnt_drv[instruccion_agente.origen]);
                     end
                     
