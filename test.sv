d8358b8a43c3d5e0276da8e3c0036aa4a03482cd
Petecosh <prodrguez4@gmail.com>
2024-09-19 19:19:41 -0600

    2 DRIVERS

--- test.sv
+++ test.sv
@@ -50,7 +50,7 @@ class test #(parameter devices = 4, parameter width = 16);
         #10
         instruccion_agente = new();
         instruccion_agente.tipo = lectura;
-        instruccion_agente.origen = 2;
+        instruccion_agente.origen = 0;
         instruccion_agente.print("Test: Paquete al agente creado");
         test_agnt_mbx.put(instruccion_agente);
 
