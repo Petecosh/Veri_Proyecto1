4378ca6adf48acb34b9e9bb0fa7a71a43bf24bea
Petecosh <prodrguez4@gmail.com>
2024-09-23 14:16:49 -0600

    Update driver.sv

--- driver.sv
+++ driver.sv
@@ -57,13 +57,11 @@ class driver #(parameter width = 16);
     $display("sip actufifoi run");
         forever begin
             // Si DUT pide pop 
-            //@(posedge vif.clk);
-            //if (vif.pop[0][id]) begin
-            if (pop_DUT) begin
-                //bit [width-1:0] auxiliar = emul_fifo_i.pop_front();
-                //vif.D_pop[0][id] = auxiliar;
-                //vif.D_pop = emul_fifo_i.pop_front();
-                dato_i_DUT = emul_fifo_i.pop_front();
+            @(posedge vif.clk);
+            if (vif.pop[0][id]) begin
+                bit [width-1:0] auxiliar = emul_fifo_i.pop_front();
+                vif.D_pop[0][id] = auxiliar;
+                vif.D_pop = emul_fifo_i.pop_front();
                 $display("[%g] Driver FIFO in: DUT saco dato", $time);
             end
         end
@@ -73,11 +71,9 @@ class driver #(parameter width = 16);
     $display("sip fifoo run");
         forever begin
             // Si DUT pide push
-            //@(posedge vif.clk);
-            //if (vif.push[0][id]) begin
-            if (push_DUT) begin
-                //emul_fifo_o.push_back(vif.D_push[0][id]);
-                emul_fifo_o.push_back(dato_o_DUT);
+            @(posedge vif.clk);
+            if (vif.push[0][id]) begin
+                emul_fifo_o.push_back(vif.D_push[0][id]);
                 $display("[%g] Driver FIFO out: DUT metio dato", $time);
             end
         end
@@ -87,15 +83,13 @@ class driver #(parameter width = 16);
     $display("sip fifoi run");
         // Revisar si hay algo pendiente en la FIFO entrada
         forever begin
-            //@(posedge vif.clk);
+            @(posedge vif.clk);
             if (emul_fifo_i.size() != 0) begin
-                    //vif.pndng[0][id] = 1;
-                    pending = 1;
+                    vif.pndng[0][id] = 1;
                     //$display("[%g] Driver FIFO in: Pending en 1", $time);
                         
             end else begin
-                    //vif.pndng[0][id] = 0;
-                    pending = 0;
+                    vif.pndng[0][id] = 0;
                     //$display("[%g] Driver FIFO in: Pending en 0", $time);
             end
         end
