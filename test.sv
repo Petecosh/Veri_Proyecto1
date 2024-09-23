95f0cfefd26d2081aeea0f38e2afef0b4a77e374
Josue096 <josu3qb@gmail.com>
2024-09-23 14:59:12 -0600

    p de prueba

--- test.sv
+++ test.sv
@@ -27,8 +27,8 @@ class test #(parameter bits = 1, parameter devices = 4, parameter width = 16, pa
         // Pruebas
         #10
         instruccion_agente = new();
-        instruccion_agente.randomize();
-        //instruccion_agente.dato=16'h5;
+        //instruccion_agente.randomize();
+        instruccion_agente.dato=16'h103;
         instruccion_agente.origen=2;
         instruccion_agente.tipo = Random;
         instruccion_agente.print("Test: Paquete al agente creado");
