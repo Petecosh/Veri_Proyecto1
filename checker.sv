af233250a38a5b49abed178f19627a471aaf7453
Josue096 <josu3qb@gmail.com>
2024-09-25 00:29:18 -0600

    ish perro

--- checker.sv
+++ checker.sv
@@ -21,6 +21,7 @@ class checkr #(parameter width = 16, parameter devices = 4);
 
         forever begin
             for (int h=0; h<devices;h++)begin
+                #20
                 if (drv_chkr_mbx[h].num()>0)begin
                     pck_drv_chkr #(.width(width)) paquete_chkr;
                     drv_chkr_mbx[h].get(paquete_chkr);
