15425562c584d5701199b9a55a0f9bc666c7dc42
Josue096 <josu3qb@gmail.com>
2024-09-24 22:58:36 -0600

    ish perro

--- checker.sv
+++ checker.sv
@@ -3,12 +3,14 @@ class checkr #(parameter width = 16, parameter devices = 4);
     tipo_mbx_drv_chkr drv_chkr_mbx;
     pck_drv_chkr keys[$];
     int index[$];
-
+    int contador1;
+    int contador0;
     
     function new();
         this.keys = {};
         this.index = {};
-        this.index = 0;
+        this.contador0 = 0;
+        this.contador1 = 0;
 
     endfunction
     task run();
@@ -23,24 +25,24 @@ class checkr #(parameter width = 16, parameter devices = 4);
             else i = 0;
             */
             drv_chkr_mbx.get(paquete_chkr);
-            case (paquete_chkr.origen)
+            case (paquete_chkr.accion)
                  
                 1'b0: begin
                     
-                    for (int i = 0; i =< index.size(); i++) begin
-                            index[i] = paquete_chkr.origen; 
-                            keys[i] = paquete_chkr;
-                    end
+                    index[contador0] = paquete_chkr.origen; 
+                    keys[contador0] = paquete_chkr;
+                    contador0++;
                 
                 end
                 1'b1: begin
                     
-                    for (int j = 0; j =< keys.size(); j++) begin
+                    for (int j = 0; j =< contador0; j++) begin
                             if (keys[j] == paquete_chkr)begin
                                 to_sc[contador]=index[j];
                                 $display("[%g] dato checkaeado org = %h, dato%h", $time,index[j],keys[j]);
                                 index.delete(j);
                                 keys.delete(j);
+                                contador0 = contador0-1;
                             end
                     end
                 
