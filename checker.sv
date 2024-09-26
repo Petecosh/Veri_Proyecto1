84ca14748f439c9a883b7cd577a1f3163733ddd4
Josue096 <josu3qb@gmail.com>
2024-09-26 12:32:53 -0600

    broadcast

--- checker.sv
+++ checker.sv
@@ -22,7 +22,7 @@ class checkr #(parameter width = 16, parameter devices = 4);
         forever begin
             for (int h=0; h<devices;h++)begin
                 #20
-                $display("[%g] cantidad de mensajes %d en el mail%d",$time,drv_chkr_mbx[h].num(),h);
+                $display("[%g] ccontador %d en el mail%d",$time,contador0);
                 if (drv_chkr_mbx[h].num()>0)begin
                     pck_drv_chkr #(.width(width)) paquete_chkr;
                     drv_chkr_mbx[h].get(paquete_chkr);
