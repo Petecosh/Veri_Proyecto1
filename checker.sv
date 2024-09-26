d15f7ecd6105ac4b30efb768679f8b00122adf3a
Petecosh <prodrguez4@gmail.com>
2024-09-26 17:45:22 -0600

    PRUEBITAS

--- checker.sv
+++ checker.sv
@@ -25,56 +25,71 @@ class checkr #(parameter width = 16, parameter devices = 4);
         $display("[%g] Chekcer inicializado", $time);
 
         forever begin
-            for (int h=0; h<devices;h++)begin
+
+            for (int h = 0; h < devices; h++)begin
+
                 #20
-                if (drv_chkr_mbx[h].num()>0)begin
+                if (drv_chkr_mbx[h].num() > 0)begin
+
                     pck_drv_chkr #(.width(width)) paquete_chkr;
                     drv_chkr_mbx[h].get(paquete_chkr);
-                    case (paquete_chkr.accion)
-                        
-                        1'b0: begin
-                            $display("[%g] Checker recive: org = %h, dato%h", $time,paquete_chkr.origen,paquete_chkr.dato);
+
+                    if (paquete_chkr.dato[width-1:width-8] == h) begin
+
+                        $display("[%g] Dato recibido en Driver correcto", $time);
+
+                        case (paquete_chkr.accion)
                             
-                            if (paquete_chkr.dato[width-1:width-8] == 8'hffff) begin
-                                for (int i = 0; i < devices-1; i++) begin
+                            1'b0: begin
+
+                                $display("[%g] Checker recibe: org = %h, dato%h", $time, paquete_chkr.origen, paquete_chkr.dato);
+                                
+                                if (paquete_chkr.dato[width-1:width-8] == 8'hffff) begin
+                                    for (int i = 0; i < devices-1; i++) begin
+                                        index[con_index] = paquete_chkr.origen; 
+                                        keys[con_index] = paquete_chkr;
+                                    con_index++;
+                                    end
+                                end
+
+                                else if (paquete_chkr.dato[width-1:width-8] < devices) begin
                                     index[con_index] = paquete_chkr.origen; 
                                     keys[con_index] = paquete_chkr;
-                                con_index++;
+                                    con_index++;
+                                end
+
+                                else begin
+                                    $display("[%g] Dato con direccion erronea: org = %h, dato =%h", $time,paquete_chkr.origen,paquete_chkr.dato);
+                                    Procesos_erroneos[con_err] = paquete_chkr.dato;
+                                    con_err++;
+                                end
+                                
+                            end
+                            1'b1: begin
+                            for (int j = 0; j < con_index; j++) begin  
+                                    if (keys[j].dato == paquete_chkr.dato)begin
+                                        $display("[%g] Dato checkaeado: org = %h, dato%h", $time,index[j],keys[j].dato);
+                                        index.delete(j);
+                                        keys.delete(j);
+                                        con_index = con_index-1;
+                                        check_correcto = 1'b1;
+                                    end                                                               
+                                end
+                                if (check_correcto == 0)begin
+                                    $display("[%g] Nadie envio ese dato: dato =%h", $time,paquete_chkr.dato);
+                                    Procesos_erroneos[con_err] = paquete_chkr.dato;
+                                    con_err++;
+                                    check_correcto = 1'b0;
                                 end
                             end
-                            else if (paquete_chkr.dato[width-1:width-8] < devices) begin
-                                index[con_index] = paquete_chkr.origen; 
-                                keys[con_index] = paquete_chkr;
-                                con_index++;
+                            default: begin
+                                $display("[%g] WHAT: org = %h, dato%h", $time,paquete_chkr.origen,paquete_chkr.dato);
                             end
-                            else begin
-                                $display("[%g] dato con direccion erronea: org = %h, dato =%h", $time,paquete_chkr.origen,paquete_chkr.dato);
-                                Procesos_erroneos[con_err] = paquete_chkr.dato;
-                                con_err++;
-                            end
-                            
-                        end
-                        1'b1: begin
-                           for (int j = 0; j < con_index; j++) begin  
-                                if (keys[j].dato == paquete_chkr.dato)begin
-                                    $display("[%g] Dato checkaeado: org = %h, dato%h", $time,index[j],keys[j].dato);
-                                    index.delete(j);
-                                    keys.delete(j);
-                                    con_index = con_index-1;
-                                    check_correcto = 1'b1;
-                                end                                                               
-                            end
-                            if (check_correcto == 0)begin
-                                $display("[%g] Nadie envio ese dato: dato =%h", $time,paquete_chkr.dato);
-                                Procesos_erroneos[con_err] = paquete_chkr.dato;
-                                con_err++;
-                                check_correcto = 1'b0;
-                            end
-                        end
-                        default: begin
-                            $display("[%g] WHAT: org = %h, dato%h", $time,paquete_chkr.origen,paquete_chkr.dato);
-                        end
-                    endcase
+                        endcase
+
+                    end else begin
+                        $display("[%g] Dato en Driver INCORRECTO", $time);
+                    end
                 end
             end
         end
