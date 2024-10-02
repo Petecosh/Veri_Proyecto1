6712498c05e2082bc11b1b362d67b84ca579816b
Petecosh <prodrguez4@gmail.com>
2024-10-01 21:06:28 -0600

    Update test_bench.sv

--- test_bench.sv
+++ test_bench.sv
@@ -14,10 +14,10 @@ module test_bench;
 
     reg clk;
     parameter width = 16;
-    parameter devices = 6;
+    parameter devices = 8;
     parameter bits = 1;
     parameter broadcast = {8{1'b1}};
-    test #(.bits(bits), .devices(6), .width(width), .broadcast(broadcast)) test_inst;          // Instancia del test
+    test #(.bits(bits), .devices(devices), .width(width), .broadcast(broadcast)) test_inst;          // Instancia del test
 
     bus_if #(.bits(bits), .drvrs(devices), .pckg_sz(width), .broadcast(broadcast)) _if(.clk(clk));   // Interfaz
 
