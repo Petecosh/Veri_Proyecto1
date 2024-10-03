7016064514e4622010b078ee2270ad9112c85c52
Petecosh <prodrguez4@gmail.com>
2024-10-03 14:54:42 -0600

    Debuggear

--- test_bench.sv
+++ test_bench.sv
@@ -11,11 +11,6 @@
 module test_bench;
 
     reg clk;                          // Variable para el clock
-
-    pck_aleatorio paquete_aleatorio;  // Paquete aleatorio para devices y width
-    paquete_aleatorio = new();        // Inicializar el paquete
-    paquete_aleatorio.randomize();    // Aleatorizar los valores de devices y width
-
     parameter width = 32;             // Parametro del ancho del paquete
     parameter devices = 5;            // Parametro de cantidad de dispositivos en el DUT
     parameter bits = 1;           
@@ -41,6 +36,9 @@ module test_bench;
 
     initial begin
         clk = 0;                                                     // Clock en 0
+        pck_aleatorio paquete_aleatorio;  // Paquete aleatorio para devices y width
+        paquete_aleatorio = new();        // Inicializar el paquete
+        paquete_aleatorio.randomize();    // Aleatorizar los valores de devices y width
         $display("[%g] Devices = %0d, Width = %0d", $time, paquete_aleatorio.devices, paquete_aleatorio.width);
         test_inst = new();                                           // Inicializar la instancia del test
         $display("[%g] Test inicializado", $time);                   
